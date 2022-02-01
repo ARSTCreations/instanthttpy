0edb5f01e9f24b206842ee9d39bc748d6f691be9
ARSTCreations <anandarizaldyaristyo@gmail.com>
2022-02-01 23:50:27 +0700

    fixed logging and added example

--- start.sh
+++ start.sh
@@ -11,24 +11,33 @@ read choice
 
 if [[ $choice -eq 1 ]]
 then
+  date=$(date '+%Y%m%d%H%M%S')
   echo "serving on http://${ip}:8000"
-  python -m http.server --bind ${ip} 8000
+  python -u -m http.server --bind ${ip} 8000 2> log${date}.txt
+  cp log${date}.txt iplog${date}.txt
+  sed -i 's/\s.*//' iplog${date}.txt
 elif [[ $choice -eq 2 ]]
 then
+  date=$(date '+%Y%m%d%H%M%S')
   echo "insert port (1024 - 65535)"
   echo -n "${ip}:"
   read port
   echo "serving on http://${ip}:${port}"
-  python -m http.server --bind ${ip} ${port}
+  python -u -m http.server --bind ${ip} ${port} | sed 's/\s.*//' 2> log${date}.txt            
+  cp log${date}.txt iplog${date}.txt
+  sed -i 's/\s.*//' iplog${date}.txt
 elif [[ $choice -eq 3 ]]
 then
+  date=$(date '+%Y%m%d%H%M%S')
   echo -n "insert ip (format: abc.def.ghi.jkl):"
   read ip
   echo;
   echo -n "insert port ${ip}:"
   read port
   echo "serving on http://${ip}:${port}"
-  python -m http.server --bind ${ip} ${port}
+  python -u -m http.server --bind ${ip} ${port} | sed 's/\s.*//' 2> log${date}.txt            
+  cp log${date}.txt iplog${date}.txt
+  sed -i 's/\s.*//' iplog${date}.txt
 else
   echo "invalid input"
   exit
